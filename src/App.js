3e0df3793d70fc2ac1ff70fa5e59c577ce89ec24
Charlie <charlie.davismiller@gmail.com>
2021-06-09 15:53:55 +0100

    peeps rendered - peeps setState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () =>  {
+  
+  const [peeps, setPeeps] = useState();
 
-  const handleGetPeeps = () => {
-    return fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
+  useEffect(() => {
+    fetchPeepsPromise();
+  }, [])
+
+  const fetchPeepsPromise = () => {
+    fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
     .then(res => res.json())
+    .then(data => setPeeps(data.map(peep => `${peep.body}\n`)));
   }
 
   return (
     <div className="App">
       <header className="App-header">
+      <p>{peeps}</p>
       </header>
+      
     </div>
   );
 }
